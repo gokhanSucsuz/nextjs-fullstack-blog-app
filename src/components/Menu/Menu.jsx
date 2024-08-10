9a95f3a2dac7db6bfbd7aa13559270972cc03548
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-08-10 13:56:39 +0300

    now sidebar is creating

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,10 +1,60 @@
 import React from 'react'
 import styles from "./menu.module.css"
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Menu = () => {
     return (
         <div className={styles.container}>
-            Menu
+            <h2 className={styles.subTitle}>{"What's hot"}</h2>
+            <h1 className={styles.title}>Most Popular</h1>
+            <div className={styles.items}>
+                <Link href="/" className={styles.item}>
+                    <div className={styles.imgContainer}>
+                        <Image className={styles.image} src="/p1.jpeg" alt='logo' fill />
+                    </div>
+                    <div className={styles.txtContainer}>
+                        <div className={styles.category}>TRAVEL</div>
+                        <div className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, placeat?</div>
+                        <div className={styles.date}>Gökhan SUÇSUZ - 10.08.2024</div>
+
+                    </div>
+                </Link>
+                <Link href="/" className={styles.item}>
+                    <div className={styles.imgContainer}>
+                        <Image className={styles.image} src="/p1.jpeg" alt='logo' fill />
+                    </div>
+                    <div className={styles.txtContainer}>
+                        <div className={styles.category}>TRAVEL</div>
+                        <div className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, placeat?</div>
+                        <div className={styles.date}>Gökhan SUÇSUZ - 10.08.2024</div>
+
+                    </div>
+                </Link>
+                <Link href="/" className={styles.item}>
+                    <div className={styles.imgContainer}>
+                        <Image className={styles.image} src="/p1.jpeg" alt='logo' fill />
+                    </div>
+                    <div className={styles.txtContainer}>
+                        <div className={styles.category}>TRAVEL</div>
+                        <div className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, placeat?</div>
+                        <div className={styles.date}>Gökhan SUÇSUZ - 10.08.2024</div>
+
+                    </div>
+                </Link>
+                <Link href="/" className={styles.item}>
+                    <div className={styles.imgContainer}>
+                        <Image className={styles.image} src="/p1.jpeg" alt='logo' fill />
+                    </div>
+                    <div className={styles.txtContainer}>
+                        <div className={styles.category}>TRAVEL</div>
+                        <div className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, placeat?</div>
+                        <div className={styles.date}>Gökhan SUÇSUZ - 10.08.2024</div>
+
+                    </div>
+                </Link>
+            </div>
+
         </div>
     )
 }
